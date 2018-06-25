3d20f2ed3124e61b33de7d7208999122ea140e76
David Callizaya <davidcallizaya@gmail.com>
2018-06-25 14:50:03 -0700

    Test engine 1

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,9 +4,14 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Instance;
+use App\Token;
 
 class ExampleTest extends TestCase
 {
+
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      *
@@ -14,8 +19,37 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        //Inicia el proceso PROCESS_1 de bpmn/Lanes.bpmn
+        $this->artisan('bpmn:callProcess', [
+            'filename' => 'bpmn/Lanes.bpmn',
+            'process' => 'PROCESS_1',
+        ]);
+        
+        //Assertion: Verifica que se ha creado una instancia
+        $this->assertEquals(Instance::count(), 1);
+        //Assertion: Verifica que se ha creado un token
+        $this->assertEquals(token::count(), 1);
 
-        $response->assertStatus(200);
+        //Obtiene los registros de instancia y token
+        $instance = Instance::first();
+        $token = Token::first();
+
+        //Se completa el token
+        $this->artisan('bpmn:completeTask', [
+            'filename' => 'bpmn/Lanes.bpmn',
+            'process' => 'PROCESS_1',
+            'instanceId' => $instance->id,
+            'tokenId' => $token->id,
+        ]);
+
+        //Assertion: Verifica que ahora existen dos tokens
+        $this->assertEquals(2, token::count());
+        
+        //Assertion: Verifica que el primer token esta CLOSED
+        $tokens = Token::orderBy('id')->get();
+        $this->assertEquals('CLOSED', $tokens[0]->status);
+
+        //Assertion: Verifica que el segundo token esta ACTIVE
+        $this->assertEquals('ACTIVE', $tokens[1]->status);
     }
 }
